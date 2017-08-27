beac68d7c39f14c6c1ca7e6bef16973768408528
Gabewest1 <gabe_west1@yahoo.com>
2017-08-27 18:55:10 -0500

    Included eslint and refactored code to reflect linting rules.

--- index.android.js
+++ index.android.js
@@ -34,7 +34,7 @@ let NavItem = styled.Text`
 export default class Online extends Component {
   render() {
     return (
-      <View style={styles.container}>
+      <View style={ styles.container }>
         <Navbar>
           <NavItem>
               Home
@@ -49,16 +49,16 @@ export default class Online extends Component {
               Contact
           </NavItem>
         </Navbar>
-        <Text style={styles.welcome}>
+        <Text style={ styles.welcome }>
           Welcome to React Native!
         </Text>
         <Text>
           Home of the Dimesdale Dimmadome!
         </Text>
-        <Text style={styles.instructions}>
+        <Text style={ styles.instructions }>
           To get started, edit index.android.js
         </Text>
-        <Text style={styles.instructions}>
+        <Text style={ styles.instructions }>
           Double tap R on your keyboard to reload,{'\n'}
           Shake or press menu button for dev menu
         </Text>
@@ -74,18 +74,18 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#F5FCFF'
   },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
-    margin: 10,
+    margin: 10
   },
   instructions: {
     textAlign: 'center',
     color: '#333333',
-    marginBottom: 5,
-  },
+    marginBottom: 5
+  }
 });
 
 AppRegistry.registerComponent('Online', () => Online);
--- index.ios.js
+++ index.ios.js
@@ -12,42 +12,80 @@ import {
   View
 } from 'react-native';
 
+import styled from "styled-components/native"
+
+let Navbar = styled.View`
+  height: 65px;
+  background: lightblue;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-around;
+  position: absolute;
+  top: 0;
+  width: 100%;
+  font-size: 22px;
+`
+
+let NavItem = styled.Text`
+  color: #efefef;
+`
+
 export default class Online extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
+      <View style={ styles.container }>
+        <Navbar>
+          <NavItem>
+              Home
+          </NavItem>
+          <NavItem>
+              About
+          </NavItem>
+          <NavItem>
+              Projects
+          </NavItem>
+          <NavItem>
+              Contact
+          </NavItem>
+        </Navbar>
+        <Text style={ styles.welcome }>
           Welcome to React Native!
         </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
+        <Text>
+          Home of the Dimesdale Dimmadome!
         </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
+        <Text style={ styles.instructions }>
+          To get started, edit index.android.js
+        </Text>
+        <Text style={ styles.instructions }>
+          Double tap R on your keyboard to reload,{'\n'}
+          Shake or press menu button for dev menu
         </Text>
       </View>
     );
   }
 }
 
+console.log("STYLES", styles)
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#F5FCFF'
   },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
-    margin: 10,
+    margin: 10
   },
   instructions: {
     textAlign: 'center',
     color: '#333333',
-    marginBottom: 5,
-  },
+    marginBottom: 5
+  }
 });
 
 AppRegistry.registerComponent('Online', () => Online);
