24ab5bc5656a420bd3f068d4a70f49d84cde2818
Gabewest1 <gabe_west1@yahoo.com>
2017-08-30 17:42:18 -0500

    Created a circle component

---
+++ app/components/shared/Circle/index.js
@@ -0,0 +1,47 @@
+import React from "react"
+import styled from "styled-components/native"
+import PropTypes from "prop-types"
+
+export default class Circle extends React.Component {
+    render() {
+        console.log(this.props.style)
+        let {
+            left, top, color, radius, style
+        } = this.props
+
+        return (
+            <CircleView
+                style={ style }
+                color={ color }
+                radius={ radius }
+                left={ left }
+                top={ top } />
+        )
+    }
+}
+
+const CircleView = styled.View`
+    width: ${({ radius }) => radius};
+    height: ${({ radius }) => radius};
+    border-radius: ${({ radius }) => radius / 2};
+    backgroundColor: ${props => props.color};
+    ${({ top, left }) => {
+        console.log(top, left)
+        return `top: ${top}; left: ${left};`
+    }
+    }
+`
+
+Circle.propTypes = {
+    color: PropTypes.string.isRequired,
+    radius: PropTypes.number.isRequired,
+    style: PropTypes.object,
+    left: PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.number
+    ]),
+    top: PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.number
+    ])
+}
