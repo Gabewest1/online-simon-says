45b81447c89e58aa5d1c112d520eb6a256d93329
Gabriel West <gabe_west1@yahoo.com>
2017-09-16 18:38:57 -0500

    Added redux debuger middleware to store

--- app/store.js
+++ app/store.js
@@ -1,7 +1,8 @@
-import { createStore, combineReducers, applyMiddleware } from "redux"
+import { Platform } from 'react-native';
+import { createStore, combineReducers, compose, applyMiddleware } from "redux"
 import { routerReducer, routerMiddleware } from "react-router-redux"
 import createSagaMiddleware from "redux-saga"
-import { composeWithDevTools } from "redux-devtools-extension"
+import { composeWithDevTools } from "remote-redux-devtools"
 import { reducer as form } from "redux-form"
 import rootSaga from "./rootSaga"
 import reducers from "./rootReducer"
@@ -10,9 +11,19 @@ const sagaMiddleware = createSagaMiddleware()
 const reduxRouterMiddleware = routerMiddleware()
 const middlewares = [sagaMiddleware, reduxRouterMiddleware]
 
-let store = createStore(combineReducers(
-    {...reducers, router: routerReducer, form}),
-    composeWithDevTools(applyMiddleware(...middlewares))
+// const composeEnhancers = composeWithDevTools({ realtime: true, host: "192.168.1.91", port: 8081 })
+// let store = createStore(
+//     combineReducers({...reducers, router: routerReducer, form}),
+//     composeEnhancers(
+//         applyMiddleware(...middlewares),
+//     )
+// )
+
+let store = createStore(
+    combineReducers({...reducers, router: routerReducer, form}),
+    composeWithDevTools(
+        applyMiddleware(...middlewares),
+    )
 )
 
 //create store
