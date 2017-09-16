b3c132cedbaa4bcaefefa27e13352999970feb8f
Gabriel West <gabe_west1@yahoo.com>
2017-09-16 18:16:48 -0500

    Wrote tests for my sagas related to: animting the pads, performing a players turn, checking for game over.

--- __tests__/simonGame.test.js
+++ __tests__/simonGame.test.js
@@ -1,25 +1,31 @@
 import React from "react"
 import sinon from "sinon"
-import sagaHelper from "redux-saga-testing"
+import { expectSaga, testSaga } from "redux-saga-test-plan"
+import { delay } from "redux-saga"
+import { call, race, select, take } from "redux-saga/effects"
 import { shallow } from "enzyme"
 import { expect } from "chai"
 import { Reducer } from 'redux-testkit';
-import { take } from "redux-saga/effects"
 import SimonGame from "../app/components/simon__game"
 import SimonPad from "../app/components/simon__pad"
-import { actions, default as reducer } from "../app/redux/SimonSaysGame"
+import { actions, selectors } from "../app/redux/SimonSaysGame"
+import { gameReducer, movesReducer, playersReducer, padsReducer } from "../app/redux/SimonSaysGame/reducer"
 import {
-    watchGamePadClick,
-    handleGamePadClick,
-    animateSimonPad
+    ANIMATION_DURATION,
+    animateSimonPad,
+    setNextMove,
+    performPlayersTurn,
+    displayMovesToPerform
 } from "../app/redux/SimonSaysGame/sagas"
 
 /*
     Tests for the components
 */
 describe("<SimonGame />", () => {
+    let isAnimating = true
     let requiredProps = {
-        onPress: () => {}
+        onPress: () => {},
+        pads: [{ isAnimating }, { isAnimating }, { isAnimating }, { isAnimating }]
     }
     it("should render", () => {
         const wrapper = shallow(<SimonGame { ...requiredProps } />)
@@ -33,7 +39,7 @@ describe("<SimonGame />", () => {
 
     it("should pass down click events to <SimonPad /> components", () => {
         const onButtonClick = sinon.spy()
-        const wrapper = shallow(<SimonGame onPress={ onButtonClick } />)
+        const wrapper = shallow(<SimonGame { ...requiredProps } onPress={ onButtonClick } />)
         wrapper.find(SimonPad).forEach(pad => pad.simulate("press"))
         expect(onButtonClick.callCount).to.equal(4)
     })
@@ -43,16 +49,12 @@ describe("<SimonGame />", () => {
     Tests for the reducer
 */
 describe("Simon says reducers", () => {
-    let padsReducer = reducer.pads
-    let gameReducer = reducer.game
-    let movesReducer = reducer.moves
-    let playersReducer = reducer.players
 
     describe("game reducer", () => {
         const initialState = {
             round: 0,
             isGameOver: false,
-            winner: undefined,
+            winner: undefined
         }
         it("should return initial state", () => {
             Reducer(gameReducer).expect({}).toReturnState(initialState)
@@ -60,35 +62,37 @@ describe("Simon says reducers", () => {
     })
 
     describe("moves reducer", () => {
-        const initialState = {
-            moves: [],
-            moveCounter: 0
-        }
+        const initialState = []
 
         it("should return initial state", () => {
             Reducer(movesReducer).expect({}).toReturnState(initialState)
         })
-        
+
         it("should add a new move", () => {
-            let newState = { ...initialState, moves: [ ...initialState.moves, "0"] }
+            let newState = ["0"]
             Reducer(movesReducer).expect(actions.addNextMove("0")).toReturnState(newState)
         })
 
         it("should retain all previous moves when adding a new move", () => {
-            let state = { ...initialState, moves: ["2", "3", "0", "1", "2", "1"]}
-            let newState = { ...state, moves: [ ...state.moves, "3"] }
-            Reducer(movesReducer).withState(state).expect(actions.addNextMove("3")).toReturnState(newState)
+            let initialState = ["2", "3", "0", "1", "2", "1"]
+            let newState = [ ...initialState, "3"]
+            Reducer(movesReducer).withState(initialState).expect(actions.addNextMove("3")).toReturnState(newState)
         })
 
-        it("should increase the moveToPerform counter when a move is successfully validated", () => {
-            let newState = { ...initialState, moveCounter: initialState.moveCounter + 1 }
-            Reducer(movesReducer).expect(actions.increaseMoveCounter()).toReturnState(newState)
-        })
+        /*
+            These 2 tests were relavent when I kept the moves reducer state as an object
+            with a moveToPerform property. Now the moves reducer is only an array that
+            tracks which moves should be performed and doesnt care about the moveToPerform
+        */
+        // it("should increase the moveToPerform counter when a move is successfully validated", () => {
+        //     let newState = initialState.moveCounter + 1 }
+        //     Reducer(movesReducer).expect(actions.increaseMoveCounter()).toReturnState(newState)
+        // })
 
-        it("should reset moveToPerform when turn is over", () => {
-            let newState = { ...initialState, moveCounter: 0 }
-            Reducer(movesReducer).withState({ ...initialState, moveCounter: 33 }).expect(actions.resetMoveCounter()).toReturnState(newState)
-        })
+        // it("should reset moveToPerform when turn is over", () => {
+        //     let newState = 0 }
+        //     Reducer(movesReducer).withState({ ...initialState, moveCounter: 33 }).expect(actions.resetMoveCounter()).toReturnState(newState)
+        // })
     })
 
     describe("players reducer", () => {
@@ -169,13 +173,14 @@ describe("Simon says reducers", () => {
         })
 
         it("should animate the pads successfully", () => {
-            const nextState = pad => ({
-                ...initialState,
-                [pad]: {
-                    isAnimating: true,
-                    isValid: true
-                }
-            })
+            const nextState = pad => {
+                return initialState.map((p, i) => {
+                    return i == pad
+                        ? { ...p, isAnimating: true, isValid: true }
+                        : { ...p, isAnimating: false, isValid: undefined}
+                })
+            }
+
             Reducer(padsReducer)
                 .expect(actions.animateSimonPad({pad: "0", isValid: true}))
                 .toReturnState(nextState("0"))
@@ -191,13 +196,14 @@ describe("Simon says reducers", () => {
         })
 
         it("should animate the pads unsuccessfully", () => {
-            const nextState = pad => ({
-                ...initialState,
-                [pad]: {
-                    isAnimating: true,
-                    isValid: false
-                }
-            })
+            const nextState = pad => {
+                return initialState.map((p, i) => {
+                    return i == pad
+                        ? { ...p, isAnimating: true, isValid: false }
+                        : { ...p, isAnimating: false, isValid: undefined}
+                })
+            }
+
             Reducer(padsReducer)
                 .expect(actions.animateSimonPad({pad: "0", isValid: false}))
                 .toReturnState(nextState("0"))
@@ -217,24 +223,219 @@ describe("Simon says reducers", () => {
 /*
     Tests for the sagas
 */
-describe("Simon says game sagas", () => {
-    describe("watch game pad click saga", () => {
-        const it = sagaHelper(watchGamePadClick())
+describe("Single player game flow", () => {
 
-        it("should capture game pad click events", result => {
-            expect(result).to.deep.equal(take(actions.simonPadClicked))
-            return "Hello"
-        })
-        
-        it("should validate if the move was correct or incorrect", result => {
-            console.dir(result)
-            expect(result).to.equal(22)
-            return "World"
-        })
+    it("should add a new move to the array of moves to perform", () => {
+        testSaga(setNextMove)
+            .next()
+            .inspect(effect => {
+                expect(effect.PUT.action.payload).to.be.oneOf([0, 1, 2, 3])
+            })
+    })
 
-        it("should animate the simon pad", result => {
-            console.dir(result)
+    it("should animate the pads that the player needs to press before their turn", () => {
+        testSaga(displayMovesToPerform)
+            .next()
+
+            .select(selectors.getMoves)
+            .next([0, 1, 2, 3])
+
+            .call(delay, 1000)
+            .next()
+
+            .call(animateSimonPad, { pad: 0, isValid: true })
+            .next()
+
+            .call(delay, ANIMATION_DURATION)
+            .next()
             
+            .call(animateSimonPad, { pad: 1, isValid: true })
+            .next()
+
+            .call(delay, ANIMATION_DURATION)
+            .next()
+
+            .call(animateSimonPad, { pad: 2, isValid: true })
+            .next()
+
+            .call(delay, ANIMATION_DURATION)
+            .next()
+
+            .call(animateSimonPad, { pad: 3, isValid: true })
+            .next()
+
+            .call(delay, ANIMATION_DURATION)
+            .next()
+            
+            .isDone()
+    })
+
+    describe("should perform a players turn", () => {
+
+        let movesToPerform
+        let player1
+        let player2
+
+        beforeEach(() => {
+            movesToPerform = [0, 1, 3, 2]
+            player1 = {
+                id: 0,
+                name: "Gabe W.",
+                isEliminated: false,
+                isMyTurn: false
+            }
+            player2 = {
+                id: 0,
+                name: "Simon S.",
+                isEliminated: false,
+                isMyTurn: true
+            }
+        })
+        it("Case #1: player performs all moves correctly", () => {
+            testSaga(performPlayersTurn, player1)
+                .next()
+
+                .select(selectors.getMoves)
+                .next(movesToPerform)
+
+                .race({
+                    playersMove: take(actions.simonPadClicked),
+                    timedout: call(delay, 1000)
+                })
+                .next({ playersMove: actions.simonPadClicked(0)})
+
+                .call(animateSimonPad, { pad: 0, isValid: true })
+                .next()
+
+                .race({
+                    playersMove: take(actions.simonPadClicked),
+                    timedout: call(delay, 1000)
+                })
+                .next({ playersMove: actions.simonPadClicked(1)})
+
+                .call(animateSimonPad, { pad: 1, isValid: true })
+                .next()
+
+                .race({
+                    playersMove: take(actions.simonPadClicked),
+                    timedout: call(delay, 1000)
+                })
+                .next({ playersMove: actions.simonPadClicked(3)})
+
+                .call(animateSimonPad, { pad: 3, isValid: true })
+                .next()
+
+                .race({
+                    playersMove: take(actions.simonPadClicked),
+                    timedout: call(delay, 1000)
+                })
+                .next({ playersMove: actions.simonPadClicked(2)})
+
+                .call(animateSimonPad, { pad: 2, isValid: true })
+        })
+
+        it("Case #2: player performs first move wrong and is eliminated", () => {
+            testSaga(performPlayersTurn, player1)
+                .next()
+
+                .select(selectors.getMoves)
+                .next(movesToPerform)
+
+                .race({
+                    playersMove: take(actions.simonPadClicked),
+                    timedout: call(delay, 1000)
+                })
+                .next({ playersMove: actions.simonPadClicked(1)})
+
+                .put(actions.eliminatePlayer(player1))
+        })
+
+        it("Case #3: player performs some moves correctly then fails", () => {
+            testSaga(performPlayersTurn, player1)
+                .next()
+
+                .select(selectors.getMoves)
+                .next(movesToPerform)
+
+                .race({
+                    playersMove: take(actions.simonPadClicked),
+                    timedout: call(delay, 1000)
+                })
+                .next({ playersMove: actions.simonPadClicked(0)})
+
+                .call(animateSimonPad, { pad: 0, isValid: true })
+                .next()
+
+                .race({
+                    playersMove: take(actions.simonPadClicked),
+                    timedout: call(delay, 1000)
+                })
+                .next({ playersMove: actions.simonPadClicked(1)})
+
+                .call(animateSimonPad, { pad: 1, isValid: true })
+                .next()
+
+                .race({
+                    playersMove: take(actions.simonPadClicked),
+                    timedout: call(delay, 1000)
+                })
+                .next({ playersMove: actions.simonPadClicked(0)})
+
+                .put(actions.eliminatePlayer(player1))
+        })
+
+        it("Case #4: player does nothing and timesout", () => {
+            return expectSaga(performPlayersTurn, player1)
+                .provide([
+                    [select(selectors.getMoves), movesToPerform],
+                    [race({
+                        playersMove: take(actions.simonPadClicked),
+                        timedout: call(delay, 1000)
+                    }), { timedout: true }]
+                ])
+                .put(actions.eliminatePlayer(player1))
+                .run()
+        })
+
+        it("Case #5: player performs some moves then does nothing and timesout", () => {
+            return expectSaga(performPlayersTurn, player1)
+                .provide([
+                    [select(selectors.getMoves), movesToPerform],
+                    [race({
+                        playersMove: take(actions.simonPadClicked),
+                        timedout: call(delay, 1000)
+                    }), { playersMove: { payload: 0 } }],
+                    [race({
+                        playersMove: take(actions.simonPadClicked),
+                        timedout: call(delay, 1000)
+                    }), { timedout: true }]
+                ])
+                .call(animateSimonPad, { pad: 0, isValid: true })
+                .put(actions.eliminatePlayer(player1))
+                .run()
         })
     })
+
+    describe("after a player performs their turn", () => {
+        describe("if the player has lost", () => {
+            it("should save the players stats into local storage and to the database", () => {
+
+            })
+
+            it("should display a screen asking the player to restart or quit", () => {
+
+            })
+        })
+
+        describe("if the player performed their turn correctly", () => {
+            it("should increase the round counter", () => {
+                testSaga(endTurn)
+                    .next()
+
+                    .put(actions.increaseMoveCounter())
+            })
+        })
+
+    })
+
 })
