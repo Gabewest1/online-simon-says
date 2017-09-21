e2ac3a3ae19f2ee3fbd743bf6aa84fbd08fa8a54
Gabriel West <gabe_west1@yahoo.com>
2017-09-21 11:41:53 -0500

    Created a black center to display the round in the <SimonGame /> component.  Added a conditional statement for the <SimonPad /> source prop to recieve either its normal or animated image.

--- app/components/simon__game/index.js
+++ app/components/simon__game/index.js
@@ -3,42 +3,82 @@ import styled from "styled-components/native"
 import PropTypes from "prop-types"
 import SimonPad from "../simon__pad"
 
+const SIMON_GAME_DIAMETER = 320
+
 const SimonGameContainer = styled.View`
-    width: 320px;
-    height: 320px;
-    border-radius: ${320 / 2};
+    width: ${SIMON_GAME_DIAMETER}px;
+    height: ${SIMON_GAME_DIAMETER}px;
+    background-color: black;
+    border-radius: ${SIMON_GAME_DIAMETER / 2};
     align-items: center;
     justify-content: center;
     position: relative;
 `
-
+const BlackContainer = styled.View`
+    width: 330px;
+    height: 330px;
+    border-radius: ${(SIMON_GAME_DIAMETER + 10) / 2};    
+    background-color: black;
+    align-items: center;
+    justify-content: center;
+`
+const Center = styled.View`
+    width: ${SIMON_GAME_DIAMETER * .35}px;
+    height: ${SIMON_GAME_DIAMETER * .35}px;
+    border-radius: ${SIMON_GAME_DIAMETER / 2};    
+    background-color: black;
+    position: absolute;
+    align-items: center;
+    justify-content: center;
+    z-index: 3;
+`
+const Round = styled.Text`
+    color: white;
+`
 class SimonGame extends React.Component {
     render() {
         const { pads } = this.props
 
         return (
-            <SimonGameContainer>
-                <SimonPad
-                    style={{top: 0, left: 0}}
-                    source={ require("../../assets/images/game-pad-red.png") }
-                    isAnimating={ pads[0].isAnimating }
-                    onPress={ this.props.onPress } />
-                <SimonPad
-                    style={{top: 0, right: 0}}
-                    source={ require("../../assets/images/game-pad-green.png") }
-                    isAnimating={ pads[1].isAnimating }
-                    onPress={ this.props.onPress } />
-                <SimonPad
-                    style={{bottom: 0, left: 0}}
-                    source={ require("../../assets/images/game-pad-yellow.png") }
-                    isAnimating={ pads[2].isAnimating }
-                    onPress={ this.props.onPress } />
-                <SimonPad
-                    style={{bottom: 0, right: 0}}
-                    source={ require("../../assets/images/game-pad-blue.png") }
-                    isAnimating={ pads[3].isAnimating }
-                    onPress={ this.props.onPress } />
-            </SimonGameContainer>
+            <BlackContainer>
+                <SimonGameContainer>
+                    <Center>
+                        <Round>{ this.props.round }</Round>
+                    </Center>
+                    <SimonPad
+                        style={{top: 0, left: 0}}
+                        source={ pads[0].isAnimating ?
+                            require("../../assets/images/game-pad-red-active.png") :
+                            require("../../assets/images/game-pad-red.png")
+                        }
+                        isAnimating={ pads[0].isAnimating }
+                        onPress={ () => this.props.onPress(0) } />
+                    <SimonPad
+                        style={{top: 0, right: 0}}
+                        source={ pads[1].isAnimating ?
+                            require("../../assets/images/game-pad-green-active.png") :
+                            require("../../assets/images/game-pad-green.png")
+                        }
+                        isAnimating={ pads[1].isAnimating }
+                        onPress={ () => this.props.onPress(1) } />
+                    <SimonPad
+                        style={{bottom: 0, left: 0}}
+                        source={ pads[2].isAnimating ?
+                            require("../../assets/images/game-pad-yellow-active.png") :
+                            require("../../assets/images/game-pad-yellow.png")
+                        }
+                        isAnimating={ pads[2].isAnimating }
+                        onPress={ () => this.props.onPress(2) } />
+                    <SimonPad
+                        style={{bottom: 0, right: 0}}
+                        source={ pads[3].isAnimating ?
+                            require("../../assets/images/game-pad-blue-active.png") :
+                            require("../../assets/images/game-pad-blue.png")
+                        }
+                        isAnimating={ pads[3].isAnimating }
+                        onPress={ () => this.props.onPress(3) } />
+                </SimonGameContainer>
+            </BlackContainer>
         )
     }
 }
