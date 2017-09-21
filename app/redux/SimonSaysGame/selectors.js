eeffbda314dd8e840f731fbb8baa55f34fd46e15
Gabriel West <gabe_west1@yahoo.com>
2017-09-21 11:24:39 -0500

    Created selector to get the current round.

--- app/redux/SimonSaysGame/selectors.js
+++ app/redux/SimonSaysGame/selectors.js
@@ -3,3 +3,4 @@ export const getPlayers = state => state.simonSays.players
 export const getPads = state => state.simonSays.pads
 export const selectPerformingPlayer = state => state.simonSays.players[0]
 export const isGameOver = state => state.simonSays.game.isGameOver
+export const getCurrentRound = state => state.simonSays.game.round
