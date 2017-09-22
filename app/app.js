9b44b67cf4f19f54b51e06f16080ca221b25a541
Gabriel West <gabe_west1@yahoo.com>
2017-09-21 23:04:47 -0500

    Registered the Leaderboards, SelectOnlineGameMode, and FindMatchScreen screens in my app.js.

--- app/app.js
+++ app/app.js
@@ -6,7 +6,10 @@ import { Provider } from "react-redux"
 import StartingScreen from "./screens/StartingScreen"
 import SignUpScreen from "./screens/SignUpScreen"
 import LoginScreen from "./screens/LoginScreen"
+import Leaderboards from "./screens/Leaderboards"
 import SelectGameMode from "./screens/SelectGameMode"
+import SelectOnlineGameMode from "./screens/SelectOnlineGameMode"
+import FindMatchScreen from "./screens/FindMatchScreen"
 import SimonGameScreen from "./screens/SimonGameScreen"
 import SinglePlayerGameOverScreen from "./screens/SinglePlayerGameOverScreen"
 
@@ -15,7 +18,10 @@ console.log("STARTING UP MY APP")
 Navigation.registerComponent("StartingScreen", () => StartingScreen, store, Provider)
 Navigation.registerComponent("SignUpScreen", () => SignUpScreen, store, Provider)
 Navigation.registerComponent("LoginScreen", () => LoginScreen, store, Provider)
+Navigation.registerComponent("Leaderboards", () => Leaderboards, store, Provider)
 Navigation.registerComponent("SelectGameMode", () => SelectGameMode, store, Provider)
+Navigation.registerComponent("SelectOnlineGameMode", () => SelectOnlineGameMode, store, Provider)
+Navigation.registerComponent("FindMatchScreen", () => FindMatchScreen, store, Provider)
 Navigation.registerComponent("SimonGameScreen", () => SimonGameScreen, store, Provider)
 Navigation.registerComponent("SinglePlayerGameOverScreen", () => SinglePlayerGameOverScreen, store, Provider)
 
