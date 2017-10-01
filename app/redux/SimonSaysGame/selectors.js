6ea9753915059f690417ccbf2a3f7e65d3142f83
Gabriel West <gabe_west1@yahoo.com>
2017-10-01 18:44:33 -0500

    Created selectos to get the performing player and to check if the client is the performing player.

--- app/redux/SimonSaysGame/selectors.js
+++ app/redux/SimonSaysGame/selectors.js
@@ -1,7 +1,10 @@
+import { selectors as userSelectors } from "../Auth"
+
 export const getMoves = state => state.simonSays.moves
 export const getPlayers = state => state.simonSays.players
 export const getPads = state => state.simonSays.pads
-export const selectPerformingPlayer = state => state.simonSays.players[0]
+export const selectPerformingPlayer = state => state.simonSays.game.performingPlayer
+export const isItMyTurn = state => userSelectors.getUsername(state) === selectPerformingPlayer(state).username
 export const isGameOver = state => state.simonSays.game.isGameOver
 export const getCurrentRound = state => state.simonSays.game.round
 export const hasFoundMatch = state => state.simonSays.game.hasFoundMatch
