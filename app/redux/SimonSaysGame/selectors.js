25e21b2afea18cbfd9447536b6417f2a1257a46c
Gabriel West <gabe_west1@yahoo.com>
2017-10-02 15:50:08 -0500

    Updated simonGame's selectPerformingPlayer selector.

--- app/redux/SimonSaysGame/selectors.js
+++ app/redux/SimonSaysGame/selectors.js
@@ -3,7 +3,17 @@ import { selectors as userSelectors } from "../Auth"
 export const getMoves = state => state.simonSays.moves
 export const getPlayers = state => state.simonSays.players
 export const getPads = state => state.simonSays.pads
-export const selectPerformingPlayer = state => state.simonSays.game.performingPlayer
+export const selectPerformingPlayer = state => {
+    //performing player only gets set when the player is playing an
+    //online game. If there isn't a player then we know we're playing a
+    //single player game and should grab the players own user object.
+    let player = state.simonSays.game.performingPlayer
+    if (!player) {
+        player = userSelectors.getUser(state)
+    }
+
+    return player
+}
 export const isItMyTurn = state => userSelectors.getUsername(state) === selectPerformingPlayer(state).username
 export const isGameOver = state => state.simonSays.game.isGameOver
 export const getCurrentRound = state => state.simonSays.game.round
