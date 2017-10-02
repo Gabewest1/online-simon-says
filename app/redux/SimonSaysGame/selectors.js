3c358f133805ab2caa2131b72502dded632c31d7
Gabriel West <gabe_west1@yahoo.com>
2017-10-02 18:01:10 -0500

    Updated `selectPerformingPlayer` selector to return either the assigned performing player or the first player in the players array.
    Updated `isItMyTurn` selector to return false instead of undefined when there hasnt been assigned a performing player yet.

--- app/redux/SimonSaysGame/selectors.js
+++ app/redux/SimonSaysGame/selectors.js
@@ -4,17 +4,23 @@ export const getMoves = state => state.simonSays.moves
 export const getPlayers = state => state.simonSays.players
 export const getPads = state => state.simonSays.pads
 export const selectPerformingPlayer = state => {
-    //performing player only gets set when the player is playing an
-    //online game. If there isn't a player then we know we're playing a
-    //single player game and should grab the players own user object.
     let player = state.simonSays.game.performingPlayer
+
     if (!player) {
-        player = userSelectors.getUser(state)
+        player = getPlayers(state)[0]
     }
 
     return player
 }
-export const isItMyTurn = state => userSelectors.getUsername(state) === selectPerformingPlayer(state).username
+export const isItMyTurn = state => {
+    let performingPlayer = selectPerformingPlayer(state)
+
+    if (performingPlayer && userSelectors.getUsername(state) === performingPlayer.username) {
+        return true
+    } else {
+        return false
+    }
+}
 export const isGameOver = state => state.simonSays.game.isGameOver
 export const getCurrentRound = state => state.simonSays.game.round
 export const hasFoundMatch = state => state.simonSays.game.hasFoundMatch
