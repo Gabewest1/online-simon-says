58a993a9a8d2947b56765fb7bd90455c0471f959
Gabriel West <gabe_west1@yahoo.com>
2017-10-16 19:36:05 -0500

    Setup <LeaderboardScreen/> to fetch data from the server to be displayed in list form.

--- app/screens/Leaderboards/index.js
+++ app/screens/Leaderboards/index.js
@@ -1,39 +1,88 @@
 import React from "react"
 import { connect } from "react-redux"
 import { bindActionCreators } from "redux"
-import styled from  "styled-components/native"
+import { View } from "react-native"
+import styled from "styled-components/native"
+import { List, ListItem } from "react-native-elements"
 import PropTypes from "prop-types"
 
+import { BACKGROUND_COLOR } from "../../constants"
 import Background from "../../components/background"
+import Player from "../../components/player"
 
-const Container = styled(Background)`
-    justify-content: space-around;
+import { actions as leaderboardActions, selectors as leaderboardSelectors } from "../../redux/Leaderboards"
+
+const Container = styled.View`
+    flex-direction: row;
     align-items: center;
+    justify-content: space-between;
 `
-const Text = styled.Text`
 
+const Stats = styled.Text`
+`
+const RankWrapper = styled.View`
+    justify-content: center;
+    align-items: center;
+    width: 30;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+`
+const Rank = styled.Text`
+    color: black;
+    
 `
 
 class Leaderboards extends React.Component {
+    componentWillMount() {
+        this.props.fetchLeaderboardData()
+    }
     render() {
+        console.log("PLAYERSL:", this.props.players)
+
         return (
-            <Container>
-                <Text>Leaderboards</Text>
-            </Container>
+            <Background>
+                <List>
+                    {
+                        this.props.players.map((player, index) => (
+                            <ListItem
+                                key={ player.username }
+                                containerStyle={{ paddingTop: 0, paddingBottom: 0 }}
+                                hideChevron={ true }
+                                title={
+                                    <Container>
+                                        <RankWrapper>
+                                            <Rank>{ index + 1}</Rank>
+                                        </RankWrapper>
+                                        <View style={{ borderLeftWidth: 3, borderColor: BACKGROUND_COLOR, marginLeft: 30, flexDirection: "row", justifyContent: "space-between", alignItems: "center", flexGrow: 1 }}>
+                                            <Player player={ player } />
+                                            <Stats>Highscore: { player.statsByGameMode[1].bestStreak }</Stats>
+                                        </View>
+                                    </Container>
+                                } />
+                        ))
+                    }
+                </List>
+            </Background>
         )
     }
 }
 
-function mapStateToProps() {
-    return {}
+function mapStateToProps(state) {
+    return {
+        isLoading: leaderboardSelectors.isLoading(state),
+        players: leaderboardSelectors.getPlayers(state)
+    }
 }
 
-function mapDispatchToProps() {
-    return {}
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators({ ...leaderboardActions }, dispatch)
 }
 
 Leaderboards.propTypes = {
-
+    isLoading: PropTypes.bool.isRequired,
+    fetchLeaderboardData: PropTypes.func.isRequired,
+    players: PropTypes.array.isRequired
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Leaderboards)
