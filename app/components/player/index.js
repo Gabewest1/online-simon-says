131508493f1647aad791cdcc22cccebb5af86edb
Gabriel West <gabe_west1@yahoo.com>
2017-10-16 19:42:41 -0500

    Created a <Player/> component to display the players username and level within a star icon.

---
+++ app/components/player/index.js
@@ -0,0 +1,37 @@
+import React from "react"
+import { Icon } from "react-native-elements"
+import styled from "styled-components/native"
+
+const PlayerView = styled.View`
+    position: relative;
+    flex-direction: row;
+    align-items: center;
+`
+const IconView = styled.View`
+    position: relative;
+    align-items: center;
+    justify-content: center;
+`
+const Level = styled.Text`
+    position: absolute;
+    color: white;
+`
+const Name = styled.Text``
+
+class Player extends React.Component {
+    render() {
+        const { level, username } = this.props.player
+
+        return (
+            <PlayerView { ...this.props }>
+                <IconView>
+                    <Icon size={ 50 } name="star" />
+                    <Level>100</Level>
+                </IconView>
+                <Name>{ username }</Name>
+            </PlayerView>
+        )
+    }
+}
+
+export default Player
