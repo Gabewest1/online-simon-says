f62d7e4a4b8add2160333815ab236a0e9556b4b7
Gabriel West <gabe_west1@yahoo.com>
2017-10-20 02:28:39 -0500

    Added props to the <Background/> to justify the content by around or between.

--- app/components/background/index.js
+++ app/components/background/index.js
@@ -6,7 +6,10 @@ const Container = styled.View`
     background-color: ${BACKGROUND_COLOR};
     width: 100%;
     height: 100%;
-    ${({ centered }) => centered && "align-items: center; justify-content: center;"}
+    align-items: center;
+    ${({ centered }) => centered && "justify-content: center;"}
+    ${({ around }) => around && "justify-content: space-around;"}
+    ${({ between }) => between && "justify-content: space-between;"}
 `
 
 export default Container
