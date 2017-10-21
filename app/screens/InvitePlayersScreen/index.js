75474af49aef689e5a60489d94279af825afcd2b
Gabriel West <gabe_west1@yahoo.com>
2017-10-20 22:46:23 -0500

    Created <InvitePlayersScreen/>.

---
+++ app/screens/InvitePlayersScreen/index.js
@@ -0,0 +1,81 @@
+import React from "react"
+import PropTypes from "prop-types"
+import { Keyboard } from "react-native"
+import { SearchBar } from "react-native-elements"
+import { connect } from "react-redux"
+import { bindActionCreators } from "redux"
+import styled from "styled-components/native"
+import { reduxForm, Field, SubmissionError } from "redux-form"
+
+import Player from "../../components/player"
+import Background from "../../components/background"
+import MenuItem from "../../components/menu-item"
+
+import {
+    actions as simonGameActions,
+    selectors as simonGameSelectors
+} from "../../redux/SimonSaysGame"
+
+class InvitePlayersScreen extends React.Component {
+    constructor() {
+        super()
+
+        this.validate = this.validate.bind(this)
+    }
+    validate(values) {
+        Keyboard.dismiss()
+
+        values.username = values.username.toLowerCase().trim()
+
+        let errors = {}
+
+        const isUserAlreadyInGameRoom = this.props.players.find(player => player.username === values.username)
+
+        errors.username = isUserAlreadyInGameRoom ? "User already here" : undefined
+
+        if (!errors.username) {
+            this.props.invitePlayer(values.username)
+        } else {
+            throw new SubmissionError(errors)
+        }
+    }
+    render() {
+        return (
+            <Background>
+                <Field
+                    name="username"
+                    type="text"
+                    containerStyle={{ width: 280 }}
+                    component={ SearchBar }
+                    placeholder="Invite user..." />
+                <MenuItem onPress={ this.props.handleSubmit(this.validate) }>Invite</MenuItem>
+                { this.renderPlayers() }
+            </Background>
+        )
+    }
+    renderPlayers() {
+        return this.props.players.map(player =>
+            <Player player={ player } key={ player } />
+        )
+    }
+}
+
+function mapStateToProps(state) {
+    return {
+        players: simonGameSelectors.getPlayers(state)
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators({ ...simonGameActions }, dispatch)
+}
+
+InvitePlayersScreen.propTypes = {
+    handleSubmit: PropTypes.func.isRequired,
+    invitePlayer: PropTypes.func.isRequired,
+    players: PropTypes.array.isRequired
+}
+
+export default reduxForm({
+    form: "invitePlayer"
+})(connect(mapStateToProps, mapDispatchToProps)(InvitePlayersScreen))
