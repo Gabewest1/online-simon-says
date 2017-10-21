757993bb74c70daf9c80e7201ff89cf75d1f13c0
Gabriel West <gabe_west1@yahoo.com>
2017-10-21 00:22:47 -0500

    Refactored <Input/> to take a searchBar prop and render a search bar if true.

--- app/components/input/index.js
+++ app/components/input/index.js
@@ -2,6 +2,7 @@ import React from "react"
 import { Dimensions } from "react-native"
 import { FormInput, FormValidationMessage } from "react-native-elements"
 import styled from "styled-components/native"
+import { SearchBar } from "react-native-elements"
 
 const FormInputView = styled(FormInput)`
     color: white;
@@ -11,19 +12,29 @@ const View = styled.View`
     margin-left: -20;
     margin-right: -20;
 `
+const SearchBarContainer = styled.View`
+
+`
 const FormValidationMessageView = styled(FormValidationMessage)`
 
 `
 class Input extends React.Component {
     render() {
+        const { searchBar } = this.props
+
+        return searchBar ? this.renderSearchBar() : this.renderInputBar()
+    }
+    renderInputBar() {
         const { meta, placeholder, type, input: { onChange, ...restInput }} = this.props
         const shouldHideText = type === "password" && !meta.error
-        console.log("PROPS:", this.props)
 
         return (
             <View>
                 <FormInputView
                     { ...restInput }
+                    containerStyle={ this.props.containerStyle }
+                    inputStyle={ this.props.inputStyle }
+                    icon={ this.props.icon }
                     shake={ meta.error }
                     placeholderTextColor="gray"
                     underlineColorAndroid="white"
@@ -32,6 +43,27 @@ class Input extends React.Component {
                     placeholder={ placeholder } />
                 <FormValidationMessageView>{ meta.error }</FormValidationMessageView>
             </View>
+        )
+    }
+    renderSearchBar() {
+        const { meta, placeholder, type, input: { onChange, ...restInput }} = this.props
+        const shouldHideText = type === "password" && !meta.error
+
+        return (
+            <SearchBarContainer>
+                <SearchBar
+                    { ...restInput }
+                    containerStyle={ this.props.containerStyle }
+                    inputStyle={ this.props.inputStyle }
+                    icon={ this.props.icon }
+                    shake={ meta.error }
+                    placeholderTextColor="gray"
+                    underlineColorAndroid="white"
+                    onChangeText={ onChange }
+                    secureTextEntry={ shouldHideText }
+                    placeholder={ placeholder } />
+                <FormValidationMessageView>{ meta.error }</FormValidationMessageView>
+            </SearchBarContainer>
         )
     }
 }
