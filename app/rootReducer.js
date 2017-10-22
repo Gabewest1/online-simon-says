3a80351d84ac29cd451e9d8c299afbc030d4c423
Gabriel West <gabe_west1@yahoo.com>
2017-10-21 22:30:15 -0500

    Added Navigator to rootReducer and rootSaga

--- app/rootReducer.js
+++ app/rootReducer.js
@@ -1,9 +1,11 @@
 import { default as authorization} from "./redux/Auth"
 import { default as simonSays } from "./redux/SimonSaysGame"
 import { default as leaderboards } from "./redux/Leaderboards"
+import { default as navigator } from "./redux/Navigator"
 
 export default {
     authorization,
     simonSays,
-    leaderboards
+    leaderboards,
+    navigator
 }
--- app/rootSaga.js
+++ app/rootSaga.js
@@ -1,12 +1,14 @@
 import { sagas as authSagas} from "./redux/Auth"
 import { sagas as simonSaysGameSagas} from "./redux/SimonSaysGame"
 import { sagas as leaderboardsSagas} from "./redux/Leaderboards"
+import { sagas as navigatorSagas} from "./redux/Navigator"
 
 const root = function* rootSaga() {
     yield [
         authSagas(),
         simonSaysGameSagas(),
-        leaderboardsSagas()
+        leaderboardsSagas(),
+        navigatorSagas()
     ]
 }
 
