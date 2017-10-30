2afef56b911660c27386fcddbb87d82e7a0cb524
Gabriel West <gabe_west1@yahoo.com>
2017-10-30 13:59:04 -0500

    Updated <MenuItem/> to  pass down styles and invert its color scheme.

--- app/components/menu-item/index.js
+++ app/components/menu-item/index.js
@@ -1,22 +1,38 @@
 import React from "react"
+import { Dimensions } from "react-native"
 import { Button } from "react-native-elements"
 import { BACKGROUND_COLOR } from "../../constants"
 
 class MenuItem extends React.Component {
     render() {
+        const { children, icon, inverted, fontSize, onPress, style, title } = this.props
+
         const borderStyles = {
-            borderColor: "white",
-            borderRadius: 10,
+            borderColor: inverted ? BACKGROUND_COLOR : "white",
+            borderRadius: 30,
             borderWidth: .3
         }
 
-        const { children, color, icon, onPress, style, title } = this.props
+        const width = Dimensions.get("window").width
+
+        const FONT_SIZE = fontSize
+            || width > 500 ? 26
+            : 18
+
+        const color = inverted ? BACKGROUND_COLOR : "white"
 
         return (
             <Button
                 raised
-                icon={{ ...icon, style: { position: "absolute", left: "10%" } }}
-                buttonStyle={{ ...borderStyles, backgroundColor: BACKGROUND_COLOR, marginLeft: 0, marginRight: 0 }}
+                fontSize={ FONT_SIZE }
+                color={ color }
+                backgroundColor={ inverted ? "white" : BACKGROUND_COLOR }
+                icon={{ ...icon, style: { position: "absolute", left: "10%", color, fontSize: FONT_SIZE } }}
+                buttonStyle={{
+                    ...borderStyles,
+                    marginLeft: 0,
+                    marginRight: 0
+                }}
                 containerViewStyle={{ ...style, ...borderStyles, marginLeft: 0, marginRight: 0 }}
                 title={ children || title }
                 onPress={ onPress } />
