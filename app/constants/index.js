d7ceedea1594a0fe10080ec5890172d709ed2121
Gabriel West <gabe_west1@yahoo.com>
2017-11-04 02:11:23 -0500

    Updated the BACKGROUND_COLOR and added a SECONDARY_COLOR.

--- app/constants/index.js
+++ app/constants/index.js
@@ -3,4 +3,6 @@ export const TWO_PLAYER_GAME = 2
 export const THREE_PLAYER_GAME = 3
 export const FOUR_PLAYER_GAME = 4
 
-export const BACKGROUND_COLOR = "#251D27"
+// export const BACKGROUND_COLOR = "#251D27"
+export const BACKGROUND_COLOR = "#ffffff"
+export const SECONDARY_COLOR = "#595959"
