33c3815712cf7fd96873c0c9e9173369e1debe51
Gabriel West <gabe_west1@yahoo.com>
2017-11-10 19:43:13 -0600

    <BoardView/> updates if the movesArray increases.

--- app/components/BoardView/index.js
+++ app/components/BoardView/index.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import {
-  Dimensions,
-  InteractionManager,
-  StyleSheet,
-  View,
-  TouchableOpacity,
+    Dimensions,
+    InteractionManager,
+    StyleSheet,
+    View,
+    TouchableOpacity
 } from 'react-native';
 
 const Sound = require('react-native-sound');
@@ -46,107 +46,109 @@ const TILE_SIZE = CELL_SIZE - CELL_PADDING * 2;
 const LETTER_SIZE = 50;
 
 
-var mainSequence = [];
-var currSequence = [];
+let mainSequence = [];
+let currSequence = [];
 
 class BoardView extends Component {
-  constructor(props) {
-    super(props);
-    this.tiles = []
-    this.state = {lit: 0};
-  }
-
-  shouldComponentUpdate(nextProps, nextState) {
-    const tileWasPressed = this.props.lit !== nextProps.lit || this.state.lit !== nextState.lit
-    const onPressDisabledChanged = this.props.disableOnPress !== nextProps.disableOnPress
-
-    return tileWasPressed || onPressDisabledChanged
-  }
-
-  render() {
-    return (
-        <View style={styles.container}>
-            {this._renderTiles()}
-        </View>
-    )
-  }
-
-  // create four tiles
-  _renderTiles() {
-    let result = [];
-    let i = 1;
-    let bgColors = ["", "", "#3275DD", "#D93333", "#64D23B", "#FED731", "black"];
-    for (var row = 0; row < SIZE; row++) {
-      for (var col = 0; col < SIZE; col++) {
-        var position = {
-          left: col * CELL_SIZE + CELL_PADDING,
-          top: row * CELL_SIZE + CELL_PADDING
-        };
-        result.push(this._renderTile(i++, position, bgColors[i], 'white'));
-      }
-    }
-    return result;
-  }
-
-  // create one tile
-  _renderTile(id, position, bgColor, litBgColor) {
-    
-    //Is true when playing back the moves to the player or 
-    //in a multiplayer game and the opponents plays a move
-    if (this.props.lit) {
-      playAudio(this.props.lit)
+    constructor(props) {
+        super(props);
+        this.tiles = []
+        this.state = {lit: 0};
     }
 
-    const isLit = this.props.lit || this.state.lit
-    const backgroundColor = isLit == id ? litBgColor : bgColor 
+    shouldComponentUpdate(nextProps, nextState) {
+        const tileWasPressed = this.props.lit !== nextProps.lit || this.state.lit !== nextState.lit
+        const onPressDisabledChanged = this.props.disableOnPress !== nextProps.disableOnPress
+        const movesArrayIncreased = this.props.numberOfMoves !== nextProps.numberOfMoves
 
-    return (
-      <TouchableOpacity
-          style={[ styles.tile, position, { backgroundColor } ]}
-          isAnimating={ isLit }
-          activeOpacity={ this.props.disableOnPress ? 1 : .2 }
-          index={ id }
-          tile={ this.tiles[id] }
-          key={ id }
-          onPress={() => !this.props.disableOnPress && this._onPress(id) }
-          ref={ tile => this.tiles[id] = tile } />
-    )
-  }
+        return tileWasPressed || onPressDisabledChanged || movesArrayIncreased
+    }
 
-  _onPress(id) {
-    playAudio(id)
-    InteractionManager.runAfterInteractions(() => {
-      this.props.onPress(id)
-    })
-  }
+    render() {
+        return (
+            <View style={ styles.container }>
+                {this._renderTiles()}
+            </View>
+        )
+    }
+
+    // create four tiles
+    _renderTiles() {
+        let result = [];
+        let i = 1;
+        let bgColors = ["", "", "#3275DD", "#D93333", "#64D23B", "#FED731", "black"];
+        for (let row = 0; row < SIZE; row++) {
+            for (let col = 0; col < SIZE; col++) {
+                let position = {
+                    left: col * CELL_SIZE + CELL_PADDING,
+                    top: row * CELL_SIZE + CELL_PADDING
+                };
+                result.push(this._renderTile(i++, position, bgColors[i], 'white'));
+            }
+        }
+
+        return result;
+    }
+
+    // create one tile
+    _renderTile(id, position, bgColor, litBgColor) {
+
+        //Is true when playing back the moves to the player or 
+        //in a multiplayer game and the opponents plays a move
+        if (this.props.lit) {
+            playAudio(this.props.lit)
+        }
+
+        const isLit = this.props.lit || this.state.lit
+        const backgroundColor = isLit == id ? litBgColor : bgColor
+
+        return (
+            <TouchableOpacity
+                style={ [ styles.tile, position, { backgroundColor } ] }
+                isAnimating={ isLit }
+                activeOpacity={ this.props.disableOnPress ? 1 : .2 }
+                index={ id }
+                tile={ this.tiles[id] }
+                key={ id }
+                onPress={ () => !this.props.disableOnPress && this._onPress(id) }
+                ref={ tile => this.tiles[id] = tile } />
+        )
+    }
+
+    _onPress(id) {
+        playAudio(id)
+        InteractionManager.runAfterInteractions(() => {
+            this.props.onPress(id)
+        })
+    }
 
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flexDirection: "row",
-    width: CELL_SIZE * SIZE,
-    height: CELL_SIZE * SIZE,
-    backgroundColor: 'transparent',
-    justifyContent: "center",
-    alignItems: "center",
-    flexWrap: "wrap"
-  },
-  tile: {
-    position: 'absolute',
-    width: TILE_SIZE,
-    height: TILE_SIZE,
-    borderRadius: BORDER_RADIUS,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  letter: {
-    color: 'white',
-    fontSize: LETTER_SIZE,
-    backgroundColor: 'transparent',
-    borderRadius: BORDER_RADIUS,
-    textAlign: 'center',
-  },
+    container: {
+        flexDirection: "row",
+        width: CELL_SIZE * SIZE,
+        height: CELL_SIZE * SIZE,
+        backgroundColor: 'transparent',
+        justifyContent: "center",
+        alignItems: "center",
+        flexWrap: "wrap"
+    },
+    tile: {
+        position: 'absolute',
+        width: TILE_SIZE,
+        height: TILE_SIZE,
+        borderRadius: BORDER_RADIUS,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    letter: {
+        color: 'white',
+        fontSize: LETTER_SIZE,
+        backgroundColor: 'transparent',
+        borderRadius: BORDER_RADIUS,
+        textAlign: 'center'
+    }
 });
 
 export default BoardView
