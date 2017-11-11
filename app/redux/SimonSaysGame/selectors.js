ecb15cdbb99cb211128804c9ede54cba1c2430c9
Gabriel West <gabe_west1@yahoo.com>
2017-11-10 19:42:40 -0600

    Refactored SimonSaysGame selectors `numberOfMoves()` by returning the length of the moves array + 1 if its a multiplayer game.

--- app/redux/SimonSaysGame/selectors.js
+++ app/redux/SimonSaysGame/selectors.js
@@ -4,6 +4,7 @@ export const getMoves = state => state.simonSays.moves
 export const getPlayers = state => state.simonSays.players
 export const getPads = state => state.simonSays.pads
 export const getWinner = state => state.simonSays.game.winner
+export const getGameMode = state => state.simonSays.game.gameMode
 export const selectPerformingPlayer = state => {
     let player = state.simonSays.game.performingPlayer
 
@@ -32,7 +33,14 @@ export const hasFoundMatch = state => state.simonSays.game.hasFoundMatch
 export const isScreenDarkened = state => state.simonSays.game.isScreenDarkened || !isItMyTurn(state)
 export const getTimer = state => state.simonSays.game.timer
 export const hasGameStarted = state => getMoves(state).length > 0
-export const numberOfMoves = state => getMoves(state).length
+export const numberOfMoves = state => {
+    //if the game is multiplayer than the number of moves should be increased by 1
+    //b/c the performing player adds 1 additional move to the movesArray during their turn.
+    const includeOneExtraMove = getGameMode(state) > 1
+    const bonus = includeOneExtraMove ? 1 : 0
+
+    return getMoves(state).length + bonus
+}
 export const getMoveIndex = state => state.simonSays.game.moveIndex
 export const disableOnPress = state =>
     !isItMyTurn(state) || isScreenDarkened(state) || (numberOfMoves(state) > 0 && getMoveIndex(state) >= numberOfMoves(state))
