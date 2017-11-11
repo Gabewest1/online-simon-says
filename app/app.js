65f4293631300b2f3fbfca3de08ec3ef4c3fdf97
Gabriel West <gabe_west1@yahoo.com>
2017-11-10 23:44:36 -0600

    Deleted <LoginScreen/>.

--- app/app.js
+++ app/app.js
@@ -5,7 +5,6 @@ import { Provider } from "react-redux"
 
 import StartingScreen from "./screens/StartingScreen"
 import SignUpScreen from "./screens/SignUpScreen"
-import LoginScreen from "./screens/LoginScreen"
 import Leaderboards from "./screens/Leaderboards"
 import SelectGameMode from "./screens/SelectGameMode"
 import SelectOnlineGameMode from "./screens/SelectOnlineGameMode"
@@ -23,7 +22,6 @@ console.disableYellowBox = true
 
 Navigation.registerComponent("StartingScreen", () => StartingScreen, store, Provider)
 Navigation.registerComponent("SignUpScreen", () => SignUpScreen, store, Provider)
-Navigation.registerComponent("LoginScreen", () => LoginScreen, store, Provider)
 Navigation.registerComponent("Leaderboards", () => Leaderboards, store, Provider)
 Navigation.registerComponent("SelectGameMode", () => SelectGameMode, store, Provider)
 Navigation.registerComponent("SelectOnlineGameMode", () => SelectOnlineGameMode, store, Provider)
@@ -47,25 +45,3 @@ Navigation.startSingleScreenApp({
     animated: true,
     animationType: 'slide-horizontal'
 })
-// Navigation.startTabBasedApp({
-//     tabs: [
-//         {
-//             label: 'Home',
-//             screen: 'StartingScreen',
-//             title: 'Home',
-//             icon: require("./assets/images/home.png")
-//         },
-//         {
-//             label: 'Sign-up',
-//             screen: 'SignUpScreen',
-//             title: 'Sign-up',
-//             icon: require("./assets/images/home.png")
-//         },
-//         {
-//             label: 'Login',
-//             screen: 'LoginScreen',
-//             title: 'Login',
-//             icon: require("./assets/images/home.png")
-//         }
-//     ]
-// })
