55588edf458ea9d903f9c41efdb20e43fca52499
Gabriel West <gabe_west1@yahoo.com>
2017-11-12 21:16:29 -0600

    set socket PORT to production.

--- app/socket.js
+++ app/socket.js
@@ -4,8 +4,8 @@ if (!window.navigator.userAgent) {
 
 const io = require("react-native-socket.io-client/socket.io")
 
-// const PORT = "https://simon-says-online.herokuapp.com/"
-const PORT = "ws://192.168.1.91:3000"
+const PORT = "https://simon-says-online.herokuapp.com/"
+// const PORT = "ws://192.168.1.91:3000"
 const socket = io(PORT, { jsonp: false, transports: ['websocket'] })
 
 console.log("CONNECTING TO PORT:", PORT, socket)
