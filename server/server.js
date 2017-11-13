6c629cdfa8dac18911196c22ceb22f7effcc1687
Gabriel West <gabe_west1@yahoo.com>
2017-11-12 18:49:20 -0600

    Set the server to have a pingInterval === 7500ms and pingTimeout === 30000.

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ const PORT = process.env.PORT || 3000
 
 const server = app.listen(PORT, () => console.log(`running on port ${PORT}`))
 
-const io = socket(server, { "pingInterval": 10000, "pingTimeout": 5000 })
+const io = socket(server, { "pingInterval": 7500, "pingTimeout": 30000 })
 
 const gameRoomManager = new (require("./GameRoomManager"))(io)
 
