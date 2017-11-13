0491c48cbd340de24746e67a95edebf38b76d5af
Gabriel West <gabe_west1@yahoo.com>
2017-11-13 01:28:39 -0600

    Added new constant PRIVATE_MATCH.

--- app/constants/index.js
+++ app/constants/index.js
@@ -2,6 +2,7 @@ export const SINGLE_PLAYER_GAME = 1
 export const TWO_PLAYER_GAME = 2
 export const THREE_PLAYER_GAME = 3
 export const FOUR_PLAYER_GAME = 4
+export const PRIVATE_MATCH = 5
 
 // export const BACKGROUND_COLOR = "#251D27"
 export const BACKGROUND_COLOR = "#ffffff"
