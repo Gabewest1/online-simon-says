4127dedab70d1d06e5bb8c3692d874580acbc92a
Gabriel West <gabe_west1@yahoo.com>
2017-11-12 18:47:48 -0600

    Styled the <Leaderboards/> to display the clients player at the top and then the top 10 players below them.

--- app/screens/Leaderboards/index.js
+++ app/screens/Leaderboards/index.js
@@ -7,30 +7,39 @@ import { List } from "react-native-elements"
 import PropTypes from "prop-types"
 import Spinner from "react-native-spinkit"
 
-import { BACKGROUND_COLOR } from "../../constants"
+import { BACKGROUND_COLOR, SECONDARY_COLOR } from "../../constants"
 import Background from "../../components/background"
 import Player from "../../components/player"
 
 import { actions as leaderboardActions, selectors as leaderboardSelectors } from "../../redux/Leaderboards"
 import { actions as navigatorActions } from "../../redux/Navigator"
+import { selectors as userSelectors } from "../../redux/Auth"
+
+const { width } = Dimensions.get("window")
+
+const FONT_SIZE = width > 768 ? 24 : 14
 
 const Container = styled.View`
     flex-direction: row;
     align-items: center;
     justify-content: space-between;
     borderWidth: 1;
-    borderColor: black;
+    borderColor: ${ BACKGROUND_COLOR };
 `
 const PlayerStatsWrapper = styled.View`
     alignItems: center;
     borderLeftWidth: 3;
-    borderColor: ${ BACKGROUND_COLOR };
+    borderColor: ${({ borderColor }) => borderColor ? borderColor : BACKGROUND_COLOR }};
     flexDirection: row;
     justifyContent: space-between;
     padding-right: 15;
+    padding-left: 5;
     flex-grow: 1;
 `
-const Stats = styled.Text``
+const Stats = styled.Text`
+    color: ${ BACKGROUND_COLOR };
+    fontSize: ${ FONT_SIZE }
+`
 
 const RankWrapper = styled.View`
     align-items: center;
@@ -39,19 +48,29 @@ const RankWrapper = styled.View`
     width: 40;
 `
 const Rank = styled.Text`
-    color: black;
+    color: ${ BACKGROUND_COLOR };
+    fontSize: ${ FONT_SIZE }
+`
+const Sticky = styled.View`
+    position: absolute;
+    top: 0;
+    width: 100%;
 `
 const styles = StyleSheet.create({
     icon: {
-        marginHorizontal: 10,
-        marginVertical: 10
+        marginVertical: FONT_SIZE * .8,
+        borderColor: BACKGROUND_COLOR,
+        borderWidth: 1,
+        borderRadius: 50
     },
     list: {
-        backgroundColor: "white",
+        backgroundColor: SECONDARY_COLOR,
         width: Dimensions.get("window").width
     },
     name: {
-        color: "black"
+        color: BACKGROUND_COLOR,
+        fontSize: FONT_SIZE,
+        marginLeft: FONT_SIZE * .6
     }
 })
 
@@ -79,12 +98,14 @@ class Leaderboards extends React.Component {
         return true
     }
     render() {
-        console.log("PLAYERSL:", this.props.players)
+        const { isLoading, myPlayer } = this.props
+        const myPlayerStyles = { color: SECONDARY_COLOR }
 
         return (
             <Background centered={ this.props.isLoading }>
-                { this.props.isLoading
-                    ? <Spinner isVisible={ true } size={ 100 } type="FadingCircleAlt" color="white" />
+                { !isLoading && this.renderPlayer({ item: myPlayer, index: 11, style: myPlayerStyles }) }
+                { isLoading
+                    ? <Spinner isVisible={ true } size={ 100 } type="FadingCircleAlt" color={ SECONDARY_COLOR } />
                     : this.renderLeaderboard()
                 }
             </Background>
@@ -100,20 +121,20 @@ class Leaderboards extends React.Component {
             </List>
         )
     }
-    renderPlayer({ item, index }) {
+    renderPlayer({ item, index, style }) {
         console.log("PLAYER:", item)
 
         return (
             <Container key={ item.username }>
                 <RankWrapper>
-                    <Rank>{ index + 1}</Rank>
+                    <Rank style={ style }>{ index + 1}</Rank>
                 </RankWrapper>
-                <PlayerStatsWrapper>
+                <PlayerStatsWrapper borderColor={ style && style.color }>
                     <Player
                         player={ item }
-                        icon={{ style: styles.icon }}
-                        name={{ style: styles.name }} />
-                    <Stats>Highscore: { item.statsByGameMode[1].highScore }</Stats>
+                        icon={{ style: [styles.icon, style] }}
+                        name={{ style: [styles.name, style] }} />
+                    <Stats style={ style }>Highscore: { item.statsByGameMode[1].highScore }</Stats>
                 </PlayerStatsWrapper>
             </Container>
         )
@@ -123,6 +144,8 @@ class Leaderboards extends React.Component {
 function mapStateToProps(state) {
     return {
         isLoading: leaderboardSelectors.isLoading(state),
+        myHighscore: userSelectors.getHighScore(state),
+        myPlayer: userSelectors.getUser(state),
         players: leaderboardSelectors.getPlayers(state)
     }
 }
@@ -132,6 +155,8 @@ function mapDispatchToProps(dispatch) {
 }
 
 Leaderboards.propTypes = {
+    myHighscore: PropTypes.number.isRequired,
+    myPlayer: PropTypes.object.isRequired,
     isLoading: PropTypes.bool.isRequired,
     fetchLeaderboardData: PropTypes.func.isRequired,
     navigateToScreen: PropTypes.func.isRequired,
