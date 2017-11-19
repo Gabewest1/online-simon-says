549e04251dc99bb1f1a44d28bf65ab8e151b3b47
Gabriel West <gabe_west1@yahoo.com>
2017-11-18 18:15:07 -0600

    Replaced <TouchableOpacity/> with <Animated.View/> in the <SimonPad/> component.

--- app/components/SimonPad/index.js
+++ app/components/SimonPad/index.js
@@ -1,18 +1,42 @@
 import React from "react"
-import { InteractionManager, TouchableOpacity } from "react-native"
+import { Animated, InteractionManager, TouchableWithoutFeedback } from "react-native"
 import PropTypes from "prop-types"
 
-class Pad extends React.PureComponent {s
+class Pad extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            opacity: new Animated.Value(1)
+        }
+    }
+    onPressIn() {
+        this.setOpacityTo(.2, 50)
+        this.props.onPress()
+    }
+    onPressOut() {
+        this.setOpacityTo(1, 1)
+    }
+    setOpacityTo(value, duration) {
+        console.log("SET OPACITY TO")
+        Animated.timing(
+            this.state.opacity,
+            {
+                toValue: value,
+                duration,
+                useNativeDriver: true
+            }
+        ).start();
+    }
     render() {
         const { activeOpacity, key, onPress, style } = this.props
         console.log("I AM RENDERING!")
 
         return (
-            <TouchableOpacity
+            <Animated.View
                 key={ key }
-                style={ style }
-                activeOpacity={ activeOpacity }
-                onPressIn={ onPress } />
+                onTouchStart={ () => this.onPressIn() }
+                onTouchEnd={ () => this.onPressOut(1, 1) }
+                style={ [ style, { opacity: this.state.opacity }] } />
         )
     }
 }
