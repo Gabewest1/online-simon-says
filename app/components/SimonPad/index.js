9422678dc2c4f554043835488d2af1b43e1473a3
Gabriel West <gabe_west1@yahoo.com>
2017-11-20 17:59:50 -0600

    Added logic to <SimonPad/> to set its coordinates in its `onLayout` handler. Based on its `activeTouch` location, the pad will call its `onPressIn` or `onPressOut` functions.

--- app/components/SimonPad/index.js
+++ app/components/SimonPad/index.js
@@ -3,11 +3,57 @@ import { Animated, InteractionManager, TouchableWithoutFeedback } from "react-na
 import PropTypes from "prop-types"
 
 class Pad extends React.Component {
-    constructor() {
-        super()
+    constructor(props) {
+        super(props)
+        console.log("IN THE CONSTRUCTOR OF THE PAD")
         this.state = {
+            coordinates: {},
+            isAnimating: false,
             opacity: new Animated.Value(1)
         }
+    }
+    render() {
+        const { activeOpacity, activeTouch, onPress, style } = this.props
+        // console.log("I AM RENDERING!")
+
+        return (
+            <Animated.View
+                ref="pad"
+                onLayout={ this.setCoordinates }
+                style={ [ style, { opacity: this.state.opacity }] } />
+        )
+    }
+    componentWillReceiveProps(nextProps) {
+        if (this.wasPadPressed(nextProps.activeTouch) && !this.state.isAnimating) {
+            this.setState({ isAnimating: true })
+            this.onPressIn()
+        } else if (!this.wasPadPressed(nextProps.activeTouch) && this.state.isAnimating) {
+            this.setState({ isAnimating: false })
+            this.onPressOut()
+        }
+    }
+    wasPadPressed(activeTouch) {
+        const { x, y } = activeTouch
+        const { height, width, left, top } = this.state.coordinates
+
+        if (!left || !top) {
+            console.log("POSITION HASN'T BEEN SET")
+            return
+        }
+
+        const minX = left
+        const maxX = minX + width
+        const minY = top
+        const maxY = minY + height
+
+        const isBetweenTheBound = (lower, higher, val) => lower <= val && val <= higher
+
+        // console.log("X,Y:", x, y)
+        // console.log("W,H:", width, height)
+        // console.log("L,T:", minX, minY)
+        // console.log("BETWEEN BOUNDS:", isBetweenTheBound(minX, maxX, x) && isBetweenTheBound(minY, maxY, y))
+
+        return isBetweenTheBound(minX, maxX, x) && isBetweenTheBound(minY, maxY, y)
     }
     onPressIn() {
         this.setOpacityTo(.2, 50)
@@ -27,17 +73,13 @@ class Pad extends React.Component {
             }
         ).start();
     }
-    render() {
-        const { activeOpacity, key, onPress, style } = this.props
-        console.log("I AM RENDERING!")
-
-        return (
-            <Animated.View
-                key={ key }
-                onTouchStart={ () => this.onPressIn() }
-                onTouchEnd={ () => this.onPressOut(1, 1) }
-                style={ [ style, { opacity: this.state.opacity }] } />
-        )
+    setCoordinates = () => {
+        console.log("ASSIGNING POSITION", this)
+        this.refs.pad._component.measure( (fx, fy, w, h, px, py) => {
+            console.log('X offset to page: ' + px)
+            console.log('Y offset to page: ' + py)
+            this.setState({ coordinates: { height: h, width: w, left: px, top: py } })
+        })
     }
 }
 
