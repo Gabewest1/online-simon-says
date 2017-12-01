596c1267f07e9affc316faf4895eef3c6a0f0a0b
Gabriel West <gabe_west1@yahoo.com>
2017-12-01 00:37:13 -0600

    Removed the <TintedBG/> component from the <SimonGameScreen/> and made it its own directory in the components folder.

---
+++ app/components/TintedBG/index.js
@@ -0,0 +1,24 @@
+import React from "react"
+import styled from "styled-components/native"
+
+const TintedBG = styled.View`
+    width: 100%;
+    height: 100%;
+    backgroundColor: rgba(0,0,0,.7);
+    position: absolute;
+    ${({ show }) => {
+        if (show) {
+            return `
+                opacity: 1;
+                z-index: 1;
+            `
+        } else {
+            return `
+                z-index: -1;
+                opacity: 0;
+            `
+        }
+    }}
+`
+
+export default TintedBG
--- app/screens/SimonGameScreen/index.js
+++ app/screens/SimonGameScreen/index.js
@@ -8,6 +8,7 @@ import styled from "styled-components/native"
 import Player from "../../components/player"
 import Background from "../../components/background"
 import BoardView from "../../components/BoardView"
+import TintedBG from "../../components/TintedBG"
 
 import {
     actions as simonGameActions,
@@ -25,24 +26,6 @@ import { SINGLE_PLAYER_GAME, BACKGROUND_COLOR, SECONDARY_COLOR } from "../../con
 const Container = styled(Background)`
     justify-content: center;
     align-items: center;
-`
-const TintedBG = styled.View`
-    width: 100%;
-    height: 100%;
-    backgroundColor: rgba(0,0,0,.7);
-    position: absolute;
-    ${({ show }) => {
-        if (show) {
-            return `
-                opacity: 1;
-            `
-        } else {
-            return `
-                z-index: -1;
-                opacity: 0;
-            `
-        }
-    }}
 `
 const PlayersView = styled.View`
     flex-direction: row;
