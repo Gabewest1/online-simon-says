d00b7cb0726c47db517aca4895078c500b7094a7
Gabriel West <gabe_west1@yahoo.com>
2017-12-03 23:30:29 -0600

    Changed logo to the ic_launcher with the text "Cimon Online" underneath.

--- app/components/simon__logo/index.js
+++ app/components/simon__logo/index.js
@@ -1,18 +1,29 @@
 import React from "react"
+import { Dimensions } from "react-native"
 import styled from "styled-components/native"
+import { SECONDARY_COLOR } from "../../constants"
+
+const width = Dimensions.get("window").width * .5
 
 const Container = styled.View`
-
+    align-items: center;
+`
+const Text = styled.Text`
+    color: ${ SECONDARY_COLOR };
+    font-size: ${ Dimensions.get("window").width >= 768 ? 56 : 36 };
+    font-weight: 900;
 `
 const SimonLogo = styled.Image`
-
+   width: ${ width };
+   height: ${ width };
 `
 
 class SimonSaysLogo extends React.Component {
     render() {
         return (
             <Container style={ this.props.style }>
-                <SimonLogo source={ require("../../assets/images/simon_logo.png") } />
+                <SimonLogo source={ require("../../assets/images/ic_launcher.png") } />
+                <Text>Cimon Online</Text>
             </Container>
         )
     }
