9ddefff5f3c74d7e55ad86d1f6d39ee3884b66b6
Gabe West <gabewest@Gabes-Mac.local>
2017-12-04 12:47:48 -0800

    Created a HOC CustomNavbar to replace the back button for ios.

---
+++ app/screens/CustomNavbar.js
@@ -0,0 +1,25 @@
+import React from "react"
+import { Platform } from "react-native"
+import Icon from "react-native-vector-icons/FontAwesome"
+
+export default function CustomNavbar(Component) {
+    return class extends React.Component {
+        componentDidMount() {
+            if (Platform.OS === "ios") {
+                this.setNavbarForIOS()
+            }
+        }
+        setNavbarForIOS() {
+            Icon.getImageSource("chevron-left", 27).then(icon => {
+                this.props.navigator.setButtons({
+                    leftButtons: [
+                        { id: "backPress", icon, title: "Back" }
+                    ]
+                })
+            })
+        }
+        render() {
+            return <Component { ...this.props } />
+        }
+    }
+}
