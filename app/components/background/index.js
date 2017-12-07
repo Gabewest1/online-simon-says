1283ccff7fab4574da5ae2995f80d3281d88d455
Gabriel West <gabe_west1@yahoo.com>
2017-12-07 16:39:02 -0600

    Made the <Background/> use the Dimensions module to set its width and height.

--- app/components/background/index.js
+++ app/components/background/index.js
@@ -1,11 +1,14 @@
 import React from "react"
+import { Dimensions } from "react-native"
 import styled from "styled-components/native"
 import { BACKGROUND_COLOR } from "../../constants"
 
+const { height, width } = Dimensions.get("window")
+
 const Container = styled.View`
     background-color: ${({ color }) => color || BACKGROUND_COLOR};
-    width: 100%;
-    height: 100%;
+    width: ${ width };
+    height: ${ height };
     align-items: center;
     justify-content: flex-start;
     ${({ centered }) => centered && "justify-content: center;"}
